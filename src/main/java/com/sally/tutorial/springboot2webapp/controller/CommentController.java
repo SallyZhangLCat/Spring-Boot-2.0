16dc71be61b7b1bac8bbca5be461729f35c908f0
sallyZhangCat <SallyZhang0416@163.com>
2020-07-08 21:05:50 +0800

    learn how to use Stream's collect method

--- src/main/java/com/sally/tutorial/springboot2webapp/controller/CommentController.java
+++ src/main/java/com/sally/tutorial/springboot2webapp/controller/CommentController.java
@@ -2,7 +2,9 @@ package com.sally.tutorial.springboot2webapp.controller;
 
 import com.sally.tutorial.springboot2webapp.model.Comment;
 import com.sally.tutorial.springboot2webapp.model.CommentType;
+import com.sally.tutorial.springboot2webapp.model.User;
 import com.sally.tutorial.springboot2webapp.service.CommentService;
+import com.sally.tutorial.springboot2webapp.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Controller
@@ -23,7 +27,16 @@ public class CommentController {
     @GetMapping("/")
     public String index(Model model) {
         List<Comment> allCommentsForToday = commentService.getAllCommentsForToday();
-        Map<CommentType, List<Comment>> groupedComments = allCommentsForToday.stream().collect(Collectors.groupingBy());
+        //anonymous class
+        Map<CommentType, List<Comment>> groupedComments2 = allCommentsForToday.stream().collect(Collectors.groupingBy(new Function<Comment, CommentType>() {
+            public CommentType apply(Comment comment){
+                return comment.getType();
+            }
+        }));
+        //lambda expression
+        Map<CommentType, List<Comment>> groupedComments3 = allCommentsForToday.stream().collect(Collectors.groupingBy((comment) -> comment.getType()));
+        //method reference
+        Map<CommentType, List<Comment>> groupedComments = allCommentsForToday.stream().collect(Collectors.groupingBy(Comment::getType));
         return "Comments";
     }
 }
